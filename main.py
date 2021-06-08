8607b4e6c573e90f4d048393802855ee49147164
PSeskauskas <patrikas.seskauskas@ucdconnect.ie>
2021-06-08 14:06:02 +0100

    Moved case data to a pandas dataframe

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import selenium.webdriver as webdriver
 from urllib.request import urlopen as uReq
 import time
+import pandas as pd
+
+# streamlit run C:/Users/patri/CSGOCasePrices/main.py
 
 st.set_page_config(layout="wide")
 
@@ -14,12 +17,15 @@ This app retrieves CS:GO case prices and quantities from the **Steam Community M
 
 st.markdown("""
 **About**
-* **Python Libraries:** streamlit, BeautifulSoup, selenium.webdriver, time, urllib
+* **Python Libraries:** streamlit, BeautifulSoup, selenium.webdriver, time, urllib, pandas
 * **Data Source:** Steam Community Market
 """)
 
 driver = webdriver.Chrome()
-
+cases = pd.DataFrame(columns=['Case', 'Quantity', 'Price'])
+case_name = []
+quantity = []
+price = []
 for page_number in range(1, 6):
     url = 'https://steamcommunity.com/market/search?q=Case&category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Type%5B%5D=tag_CSGO_Type_WeaponCase&appid=730#p' + str(
         page_number) + '_price_desc'
@@ -37,16 +43,19 @@ for page_number in range(1, 6):
 
     for container in containers:
         # case name
-        name = list(container.find("span", {"market_listing_item_name"}))[0].strip()
+        case_name.append(list(container.find("span", {"market_listing_item_name"}))[0].strip())
 
         # number of cases
-        quantity = container.find("span", "market_listing_num_listings_qty")["data-qty"]
+        quantity.append(int(container.find("span", "market_listing_num_listings_qty")["data-qty"]))
 
         # price of one case in dollars
-        price = container.find("span", {"class": "normal_price"})
-        full_price = float(price.span["data-price"]) / 100.0
-
-        # print the details of each case to the page
-        st.markdown(str(name) + ", " + str(quantity) + ", $" + str(full_price) + "\n")
+        value = (container.find("span", {"class": "normal_price"}))
+        full_price = float(value.span["data-price"]) / 100.0
+        price.append(full_price)
 
     time.sleep(2)
+
+cases['Case'] = case_name
+cases['Quantity'] = quantity
+cases['Price'] = price
+print(cases)
